0cf8244c7c094a31330fbacd47d5daa88d8b96b5
SRGit303 <superod@outlook.com>
2023-01-10 11:47:44 +0000

    Create heading with console.log

--- index.js
+++ index.js
@@ -86,3 +86,7 @@ var finances = [
 ['Jan-2017', 138230],
 ['Feb-2017', 671099]
 ];
+
+/* Write header using 'Console.log */
+console.log("Financial Analysis")
+console.log("----------------------------")
